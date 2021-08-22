a1c285bc0c0c1746b54077173c232e2732fac07f
Ashlin Darius Govindasamy <adgrules@hotmail.com>
2021-08-22 04:36:37 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+try:
+  import pyodbc
+  print('PYODBC works 100%')
+except:
+  print('PYODBC failed to install')
+
+print('ADG says hi from Docker, please edit app.py to your requirements and specs, happy coding!')
